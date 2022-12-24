a16491d7830290647293f1c60ca353539f30b56a
Morfeu12 <omarcosta152@gmail.com>
2022-12-24 17:59:05 -0300

    Func speedup ball and func mute control

--- Assets/Scripts/moveball.cs
+++ Assets/Scripts/moveball.cs
@@ -9,8 +9,14 @@ public class moveball : MonoBehaviour
     [Header("Ball Settings")]
     private Rigidbody rb;
     public GameObject particule;
+    public float velocityMin = 250;
+    public float velocityDown = 100; 
+    public float velocityMax = 400;
+    public float velocityUp = 0.3f;
     public float velocity = 300;
     private int points = 0;
+
+    [Header("Text Settings")]
     public Text score;
     public Text Win;
     public GameObject author;
@@ -69,17 +75,21 @@ public class moveball : MonoBehaviour
 
     void LateUpdate() 
     {
-       // PlaySoundBall();
+       SpeedUpBall();
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("point")){
+        if(other.gameObject.CompareTag("point"))
+        {
             Instantiate(particule, other.gameObject.transform.position, Quaternion.identity);
             audioSource.PlayOneShot(clip, volume);
             Destroy(other.gameObject);
-            SetScore();
-                        
+            SetScore();             
+        }
+        if(other.gameObject.CompareTag("mountain"))
+        {
+            SlowDownBall();
         }
     
     }
@@ -125,155 +135,40 @@ public class moveball : MonoBehaviour
         rb.AddForce(cameraRelativeMovement * velocity * Time.deltaTime);        
     }
 
-        /*
-    private void PlaySoundBall () 
+    private void SpeedUpBall()
     {
-        float ballMoving = Input.GetAxis("Vertical");
-
-        if (ballMoving == 1 && !ballAudioController)
+        if (Input.GetAxis("Vertical") > 0)
         {
-            ballMovementAudio.PlayOneShot(clipBallMovement, volumeBallMovement);
-            
-        } 
+            if (velocity < velocityMax)
+            {
+                velocity = velocity + velocityUp;
+            } else 
+            {
+                velocity = velocityMax;
+            }
 
-        else if (ballMoving == -1) 
+            if (velocity < velocityMin)
+            {
+                velocity = velocityMin;
+            }
+            print(velocity);
+        }
+
+    }
+
+    private void SlowDownBall()
+    {
+        if (velocity > velocityMin)
         {
-            ballMovementAudio.PlayOneShot(clipBallMovement, volumeBallMovement);
+            velocity = velocity - velocityDown;
 
         } else
         {
-            ballMovementAudio.Pause();
+            velocity = velocityMin;
         }
 
 
-
-        
-        if (Input.GetKeyDown("space"))
-        {
-            print("SOM");
-        }
-
-        if (Input.GetKeyUp("space"))
-        {
-            print("--Mute--");
-        }
-
-
-        if (Input.GetKeyUp("Up") || Input.GetKeyUp("W"))
-        {
-            print("QUIET");
-        } 
+        print("bati");
     }
-         */  
-
 
 }
-
-/*
-
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-
-
-public class moveball : MonoBehaviour
-{
-    [Header("Ball Settings")]
-    private Rigidbody rb;
-    public GameObject particule;
-    public float velocity = 300;
-    private int points = 0;
-    public Text score;
-    public Text Win;
-    public GameObject author;
-
-    [Header("Component Cronometer")]
-    public Text timerText;
-    private float currentTime;
-    private int minute;
-    private Text finalTime;
-
-    void Start()
-    {  
-        rb = GetComponent<Rigidbody>();
-        
-    }
-
-    void Update()
-    {   
-        if (points < 15)
-        {
-            currentTime = currentTime += Time.deltaTime;
-
-        }
-        if(currentTime > 59)
-        {
-            currentTime = 0;
-            minute++;
-        }
-        SetTimerText();
-    }
-
-    // FixedUpdate is called bettler performace em physical changes 
-    void FixedUpdate()
-    {
-        // Vector3 position = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
-        // rb.AddForce(position * velocity * Time.deltaTime);
-        MovePlayerRelativeToCamera();
-          
-    }
-     void OnTriggerEnter(Collider other)
-     {
-        if(other.gameObject.CompareTag("point")){
-            Instantiate(particule, other.gameObject.transform.position, Quaternion.identity);
-            Destroy(other.gameObject);
-            SetScore();
-                        
-        }
-    
-    }
-
-    void SetScore()
-    {
-        points ++;
-        score.text = "Score: " + points.ToString() + " / 15";
-        if (points >= 15)
-        {
-            // finalTime.text = timerText;
-            Win.text = "YOU WIN!!!";
-            author.SetActive(true);
-        } 
-    }
-
-    private void SetTimerText()
-    {
-        timerText.text = minute.ToString("00") + ":" + currentTime.ToString("00"); 
-    }
-    
-    private void MovePlayerRelativeToCamera() 
-    {
-        // Get Player Input (ORIGINAL INPUT SYSTEM)
-        float playerVerticalInput = Input.GetAxis("Vertical");
-        float playerHorizontalInput = Input.GetAxis("Horizontal");
-
-        // Get Camera-Normalized Directional Vectors
-        Vector3 forward = Camera.main.transform.forward;
-        Vector3 right = Camera.main.transform.right;
-        forward.y = 0;
-        right.y = 0;
-        forward = forward.normalized;
-        right = right.normalized;
-
-        // Create Direction-Relative Input Vectors
-        Vector3 forwardRelativeVerticalInput = playerVerticalInput * forward;
-        Vector3 rightRelativeVerticalInput = playerHorizontalInput * right;
-
-        // Create Camera-Relative Movement
-        Vector3 cameraRelativeMovement =  forwardRelativeVerticalInput + rightRelativeVerticalInput;;
-        rb.AddForce(cameraRelativeMovement * velocity * Time.deltaTime);        
-    }
-
-
-}
-*/
